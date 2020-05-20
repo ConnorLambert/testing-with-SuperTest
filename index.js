e29890e36008d1f091de8cea59263ec970659fc6
blambert <blambert@anyarinc.com>
2020-05-20 16:45:24 -0500

    Added POST requests

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ const app = express();
 app.listen(port, () => console.log("Express is listening!"))
 
 
-// First GET Request/Test
+// First GET and POST Request/Test
 app.get('/posts/1/comments', async (req, res) => {
     fetch("https://jsonplaceholder.typicode.com/posts/1/comments")
     .then(response => response.json())
@@ -24,8 +24,34 @@ request(app)
         if (err) throw err;
     });
 
+app.post('/posts/1/comments', function (req, res) {
+    fetch('https://jsonplaceholder.typicode.com/posts/1/comments', {
+        method: 'POST',
+        body: JSON.stringify({
+            postId: 55,
+            id: 56,
+            name: 'Vlad',
+            email: "asdfjkl;@yahoo.gov",
+            body: "asd;flkja;sdlfkja;dslkfj"
+        }),
+        headers: {
+            "Content-type": "application/json; charset=UTF-8"
+        }
+        })
+        .then(response => response.json())
+        .then(json => console.log(json))
+})
+    
+request(app)
+    .post('/posts/1/comments')
+    .expect(200)
+    .expect('Content-Type', 'application/json; charset=utf-8')
+    .end(function(err, res) {
+        if (err) throw err;
+    })
 
-// Second GET Request/Test
+
+// Second GET and POST Request/Test
 app.get('/albums/1/photos', async (req, res) => {
     fetch("https://jsonplaceholder.typicode.com/albums/1/photos")
     .then(response => response.json())
@@ -40,8 +66,34 @@ request(app)
         if (err) throw err;
     })
 
+app.post('/albums/1/photos', function (req, res) {
+    fetch('https://jsonplaceholder.typicode.com/albums/1/photos', {
+        method: 'POST',
+        body: JSON.stringify({
+            albumId: 55,
+            id: 56,
+            title: 'Vlad',
+            url: "fffffff.com",
+            thumbnailUrl: "anImage"
+        }),
+        headers: {
+            "Content-type": "application/json; charset=UTF-8"
+        }
+        })
+        .then(response => response.json())
+        .then(json => console.log(json))
+    })
 
-// Third GET Request/Test
+request(app)
+.post('/albums/1/photos')
+.expect(200)
+.expect('Content-Type', 'application/json; charset=utf-8')
+.end(function(err, res) {
+    if (err) throw err;
+})
+
+
+// Third GET and POST Request/Test
 app.get('/users/1/albums', async (req, res) => {
     fetch("https://jsonplaceholder.typicode.com/users/1/albums")
     .then(response => response.json())
@@ -56,8 +108,32 @@ request(app)
         if (err) throw err;
     })
 
+app.post('/users/1/albums', function (req, res) {
+    fetch('https://jsonplaceholder.typicode.com/users/1/albums', {
+        method: 'POST',
+        body: JSON.stringify({
+            userId: 12,
+            id: 15,
+            title: "BARgalargh"
+        }),
+        headers: {
+            "Content-type": "application/json; charset=UTF-8"
+        }
+        })
+        .then(response => response.json())
+        .then(json => console.log(json))
+    })
 
-// Fourth GET Request/Test
+request(app)
+.post('/users/1/albums')
+.expect(200)
+.expect('Content-Type', 'application/json; charset=utf-8')
+.end(function(err, res) {
+    if (err) throw err;
+})
+
+
+// Fourth GET and POST Request/Test
 app.get('/users/1/todos', async (req, res) => {
     fetch("https://jsonplaceholder.typicode.com/users/1/todos")
     .then(response => response.json())
@@ -71,6 +147,31 @@ request(app)
     .end(function(err, res) {
         if (err) throw err;
     })
+
+app.post('/users/1/todos', function (req, res) {
+    fetch('https://jsonplaceholder.typicode.com/users/1/todos', {
+        method: 'POST',
+        body: JSON.stringify({
+            userId: 12,
+            id: 15,
+            title: "BARgalargh",
+            completed: false
+        }),
+        headers: {
+            "Content-type": "application/json; charset=UTF-8"
+        }
+        })
+        .then(response => response.json())
+        .then(json => console.log(json))
+    })
+
+request(app)
+.post('/users/1/todos')
+.expect(200)
+.expect('Content-Type', 'application/json; charset=utf-8')
+.end(function(err, res) {
+    if (err) throw err;
+})
 
 
 // Fifth GET Request/Test
@@ -87,3 +188,30 @@ request(app)
     .end(function(err, res) {
         if (err) throw err;
     })
+
+app.post('/users/1/posts', function (req, res) {
+    fetch('https://jsonplaceholder.typicode.com/users/1/posts', {
+        method: 'POST',
+        body: JSON.stringify({
+            userId: 12,
+            id: 15,
+            title: "BARgalargh",
+            body: "The body of some thing."
+        }),
+        headers: {
+            "Content-type": "application/json; charset=UTF-8"
+        }
+        })
+        .then(response => response.json())
+        .then(json => console.log(json))
+    })
+
+request(app)
+    .post('/users/1/posts')
+    .expect(200)
+    .expect('Content-Type', 'application/json; charset=utf-8')
+    .end(function(err, res) {
+        if (err) throw err;
+    })
+
+
